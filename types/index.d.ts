744504abfae976cc418939a96ec0a207755e40d1
Clyng57 <clyng57@gmail.com>
2022-05-24 16:52:57 -0500

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -12,15 +12,15 @@ declare class utils {
   }
   static promiseMapAll: (arr: any, cb: any) => Promise<any[]>
   static promiseMapAny: (arr: any, cb: any) => Promise<any>
-  static async asyncSortObjects: (arr: string[], keyOfObj: string) => Promise<string[]>
+  static asyncSortObjects: (arr: string[], keyOfObj: string) => Promise<string[]>
 
-  static each: (array:Array, callback:(element: any, index: number) => void) => Array
-  static map: (array:Array, callback:(element: any, index: number) => any) => Array
-  static pack: (array:Array, callback:(element: any, index: number) => any) => Promise<Array>
-  static reduce: (array:Array, callback:(element: any, index: number) => void, output: any) => any
-  static toObject: (array:Array, key: string) => object
-  static filter: (array:Array, callback:(element: any, index: number) => any) => Array
-  static range: (array:Array, start: number, end: number) => Array
+  static each: (array:Array<any>, callback:(element: any, index: number) => void) => Array<any>
+  static map: (array:Array<any>, callback:(element: any, index: number) => any) => Array<any>
+  static pack: (array:Array<any>, callback:(element: any, index: number) => any) => Promise<Array<any>>
+  static reduce: (array:Array<any>, callback:(element: any, index: number) => void, output: any) => any
+  static toObject: (array:Array<any>, key: string) => object
+  static filter: (array:Array<any>, callback:(element: any, index: number) => any) => Array<any>
+  static range: (array:Array<any>, start: number, end: number) => Array<any>
 }
 
 export = utils
